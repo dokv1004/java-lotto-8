38ac83409d4bbd104e48e1d4813610a5423e4dd3
devJSH1004 <devJSH1004@gmail.com>
2025-11-03 21:48:00 +0900

    [FEAT] 당첨 번호 비교 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoInput lottoInput = new LottoInput();
-        lottoInput.InputAmount();
-        lottoInput.InputWinNumberBonus(lottoInput.InputWinNumber());
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,17 +1,57 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class LottoController {
 
     private int bonusNumber;
     private int lottoCnt;
     private List<Integer> winNumber;
+    private List<Lotto> lottoNumbers = new ArrayList<>();
+    LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
 
     public void run() {
         LottoInput lottoInput = new LottoInput();
         lottoCnt = lottoInput.InputAmount();
+        getLottoNumber(lottoCnt);
         winNumber = lottoInput.InputWinNumber();
         bonusNumber = lottoInput.InputWinNumberBonus(winNumber);
+
+    }
+
+    public List<Lotto> getLottoNumber(int lottoCnt) {
+        System.out.println(lottoCnt + "개를 구매했습니다.");
+        for (int i=0; i<lottoCnt; i++) {
+            lottoNumbers.add(lottoNumberGenerator.generateLotto());
+        }
+        for (Lotto lotto : lottoNumbers) {
+            List<Integer> numbers = new ArrayList<Integer>();
+            numbers.addAll(lotto.getNumbers());
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
+        return lottoNumbers;
+    }
+
+    public Map<LottoRanking, Integer> lottoResult(List<Lotto> lottoList, List<Integer> win) {
+        Map<LottoRanking, Integer> result = new HashMap<>();
+        for (LottoRanking rank : LottoRanking.values()) {
+            result.put(rank, 0);
+        }
+        for (Lotto lotto : lottoList) {
+            int count = 0;
+            for (int number : win) {
+                if (lotto.getNumbers().contains(number)) {
+                    count++;
+                }
+            }
+            LottoRanking rank = LottoRanking.valueOf(count, containBonusNumber(lotto.getNumbers()));
+            result.put(rank, result.get(rank) + 1);
+        }
+        return result;
+    }
+
+    public boolean containBonusNumber(List<Integer> numbers) {
+        return numbers.contains(bonusNumber);
     }
 }
